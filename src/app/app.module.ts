d814023eeb7c5e2df9797fae702b5334dc6bdc04
Marcus Guimaraes <mguimara@amazon.com>
2019-07-15 15:27:36 -0400

    first page layout and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ItemComponent } from './item/item.component';
+import { ItemViewComponent } from './item-view/item-view.component';
+import { SetViewComponent } from './set-view/set-view.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { MaterialComponent } from './material/material.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ItemComponent,
+    ItemViewComponent,
+    SetViewComponent,
+    ShoppingListComponent,
+    MaterialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/set-view/set-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-view',
+  templateUrl: './set-view.component.html',
+  styleUrls: ['./set-view.component.css']
+})
+export class SetViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
