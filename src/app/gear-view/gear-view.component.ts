741b261bad1030ae2ce07c4b8d854ed6c3754b71
Marcus Guimaraes <mguimara@amazon.com>
2019-07-18 13:37:27 -0400

    fix format of gear cards

--- src/app/gear-view/gear-view.component.ts
+++ src/app/gear-view/gear-view.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GearViewComponent implements OnInit {
 
+  gearIds: number[] = [1, 5, 106, 538];
+
   constructor() { }
 
   ngOnInit() {
