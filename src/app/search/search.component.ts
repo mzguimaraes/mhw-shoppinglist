3cbd90a5dae37c44ae1deaff479555ca27c1f1fb
Marcus Guimaraes <mguimara@amazon.com>
2019-07-19 15:34:58 -0400

    search functional

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { GearService } from '../gear.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  // TODO: 
+    // search by:
+      // name
+      // skill
+      // multiple filters
 
-  constructor() { }
+  // gearIds$: number[]; // should be Observable
+  gearIds$: Observable<number[]>;
+  
+  private nameSearchTerm = new Subject<string>();
+
+  constructor(
+    private gearService: GearService,
+  ) { }
+
+  searchByName(term: string) {
+    this.nameSearchTerm.next(term);
+  }
 
   ngOnInit() {
+    this.gearIds$ = this.nameSearchTerm.pipe(
+
+      debounceTime(300),
+
+      distinctUntilChanged(),
+
+      //TODO: get observable from search service
+      switchMap((term: string) => {
+        if (term)
+          return this.gearService.searchByName(term)
+        else return of ([]);
+      }),
+    );
   }
 
 }
